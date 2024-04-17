0916cac3885eec49d4c315d6d9a71e574813b335
ajgolez <antonius.golez@cyara.com>
2024-04-17 21:11:16 +1000

    Multiselect dropdown component

--- src/components/customtags/CustomTags.tsx
+++ src/components/customtags/CustomTags.tsx
@@ -1,19 +1,55 @@
+
 import {
-    Button,
-    DropDownButton,
-    ButtonProps,
-    DropDownButtonProps,
-} from '@progress/kendo-react-buttons'
-import { forwardRef } from 'react'
+    ListItemProps,
+    MultiSelect,
+} from "@progress/kendo-react-dropdowns";
+import React, { cloneElement } from "react";
+import { filterBy } from "@progress/kendo-data-query";
+import countries from "./countries";
 
-export const CustomTags = forwardRef(
-    (forwardedRef) => {
+/**
+ * A component that renders a MultiSelect with custom checkbox items.
+ * Allows filtering countries via an input field.
+ */
+export const CustomTags = () => {
+    // State to hold the filtered or initial list of countries
+    const [data, setData] = React.useState(countries.slice());
 
-
-        return (
+    // It renders a checkbox with each country and maps over children for additional customization.
+    const itemRender = (
+        li: React.ReactElement<HTMLLIElement>,
+        itemProps: ListItemProps
+    ) => {
+        const itemChildren = (
             <>
-                <Button title='test' fillMode={'solid'} themeColor={'primary'} size={'small'}>test</Button>
+                <input
+                    type="checkbox"
+                    name={itemProps.dataItem}
+                    checked={itemProps.selected}
+                />
+                {React.Children.map(li.props.children, child => child)}
             </>
-        )
-    }
-)
+        );
+        return cloneElement(li, {}, itemChildren);
+    };
+
+    // Handles the filter change event to filter the list of countries based on user input.
+    const handleFilterChange = (event: any) => {
+        setData(filterBy(countries.slice(), event.filter));
+    };
+
+    return (
+        <div className="example-wrapper">
+            <p>Select European countries:</p>
+            <MultiSelect
+                data={data}
+                itemRender={itemRender}
+                style={{ width: "300px" }}
+                placeholder="e.g. Austria"
+                filterable={true}
+                adaptive={true} // Enables responsive behavior for the MultiSelect
+                onFilterChange={handleFilterChange}
+            />
+        </div>
+    )
+}
